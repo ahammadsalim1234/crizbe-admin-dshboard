dc2301fd57d3e6034150ae4edce51eb3d47f1b5f
swalih-oraak <swalih.oraak@gmail.com>
2026-01-22 23:11:45 +0530

    particles added

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,5 +1,36 @@
+import { ImageParticles } from '@/components/user/ImageParticles';
 import HomePage from '../_components/HomePage';
 
 export default function Home() {
-    return <HomePage />;
+    return (
+        <>
+            <HomePage />
+            <ImageParticles
+                className="absolute inset-0"
+                images={[
+                    '/images/user/almond-1.png',
+                    '/images/user/pista-1.png',
+                    '/images/user/hazelnut-1.png',
+                    '/images/user/almond-2.png',
+                    '/images/user/pista-2.png',
+                    '/images/user/hazelnut-2.png',
+                    '/images/user/almond-3.png',
+                    '/images/user/pista-3.png',
+                    '/images/user/hazelnut-3.png',
+                    '/images/user/almond-4.png',
+                    '/images/user/pista-4.png',
+                    '/images/user/hazelnut-4.png',
+                    '/images/user/almond-5.png',
+                    '/images/user/pista-5.png',
+                    '/images/user/hazelnut-5.png',
+                    '/images/user/almond-6.png',
+                    '/images/user/pista-6.png',
+                    '/images/user/hazelnut-6.png',
+                ]}
+                quantity={18}
+                size={30}
+                staticity={12}
+            />
+        </>
+    );
 }
