ea001e181d6c9a28510265773cb2023fe26a5119
ahammadsalim1234 <ajmals.oraak@gmail.com>
2026-01-27 18:08:35 +0530

    bg change to user layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,13 +58,13 @@ export default function RootLayout({
                 className={`${interTight.variable} ${geistMono.variable} ${bricolage.variable} antialiased`}
             >
                 {children}
-                <Image
+                {/* <Image
                     src="/images/user/crizbe-bg.png"
                     alt="Crizbe"
                     width={100}
                     height={100}
                     className="fixed w-full bottom-0 pointer-events-none z-0"
-                />
+                /> */}
             </body>
         </html>
     );
